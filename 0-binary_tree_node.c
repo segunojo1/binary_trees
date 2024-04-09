ace0fd07d00694737e8a958d7601495de903e636
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 00:51:04 +0100

    0BINARY

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,16 +2,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode;
-    
-    newnode = malloc(sizeof(binary_tree_t));
-    if (!newnode)
+    if (parent == NULL)
     {
-        return (NULL);
+        binary_tree_t *newnode;
+
+        newnode = malloc(sizeof(binary_tree_t));
+        if (!newnode)
+        {
+            return (NULL);
+        }
+
+        newnode->n = value;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        return (newnode);
     }
-    
-    newnode->n = value;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    return (newnode);
 }
