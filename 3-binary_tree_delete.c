700862e5a2adba4aaf9a51f4fb84b67f89ca35dc
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 03:47:53 +0100

    DELETE NODE

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes the binary tree
+ * @tree: the pointer to root
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+    tree = NULL;
+}
