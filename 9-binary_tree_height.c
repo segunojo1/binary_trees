449ae28e309e42448250ca7c8e8b850644e69124
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 23:28:46 +0100

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,6 +5,11 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    if (!tree)
+    {
+        return (-1);
+    }
+    
     size_t left_height = binary_tree_height(tree->left);
     size_t right_height = binary_tree_height(tree->right);
 
