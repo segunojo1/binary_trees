350f0da1b9332087e56211fa84a3edf732bcfa08
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 23:39:43 +0100

    edit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,16 +2,17 @@
 /**
  * binary_tree_height - return height of the tree
  * @tree: the pointer to the root
-*/
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     if (!tree)
     {
         return (0);
     }
-    
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
-
-    return (left_height > right_height ? left_height + 1 : right_height + 1);
+    else
+    {
+        size_t left_height = binary_tree_height(tree->left);
+        size_t right_height = binary_tree_height(tree->right);
+        return (left_height > right_height ? left_height + 1 : right_height + 1);
+    }
 }
