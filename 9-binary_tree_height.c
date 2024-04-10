512d71d78e78c36d03e4c21801457c143d278b90
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 23:54:42 +0100

    fix errs

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,14 +5,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_height;
+    size_t right_height;
+
     if (tree == NULL)
     {
         return (0);
     }
     else
     {
-        size_t left_height = binary_tree_height(tree->left);
-        size_t right_height = binary_tree_height(tree->right);
+        left_height = binary_tree_height(tree->left);
+        right_height = binary_tree_height(tree->right);
         return (left_height > right_height ? left_height + 1 : right_height + 1);
     }
     return (0);
