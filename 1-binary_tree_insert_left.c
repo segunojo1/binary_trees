a14f848437cbbf760c050a15905b14dd098f1a32
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 03:08:50 +0100

    brackets

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
     if (!parent)
-        return NULL;
+        return (NULL);
 
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
