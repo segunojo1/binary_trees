64d866823d6ae2c35a04e5403a8af97cf46d86f5
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 01:40:51 +0100

    push

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
 /**
  * struct bin_tree - binary tree struct
@@ -8,45 +8,45 @@
  * @left: pointer to the left node
  * @right: pointer to right node
 */
-struct bin_tree {
-    int data;
-    struct bin_tree *left;
-    struct bin_tree *right;
-};
+// struct bin_tree {
+//     int data;
+//     struct bin_tree *left;
+//     struct bin_tree *right;
+// };
 
-typedef struct bin_tree bin_tree_t;
-bin_tree_t *create_bin_tree();
-void main()
-{
-    bin_tree_t *root;
-    root = create_bin_tree();
-};
-/**
- * create_bin_tree - creates a binary tree 
- * 
-*/
-bin_tree_t *create_bin_tree() {
-    bin_tree_t *new_node;
-    int num;
+// typedef struct bin_tree bin_tree_t;
+// bin_tree_t *create_bin_tree();
+// void main()
+// {
+//     bin_tree_t *root;
+//     root = create_bin_tree();
+// };
+// /**
+//  * create_bin_tree - creates a binary tree 
+//  * 
+// */
+// bin_tree_t *create_bin_tree() {
+//     bin_tree_t *new_node;
+//     int num;
 
-    new_node = (bin_tree_t *)malloc(sizeof(bin_tree_t));
-    if (!new_node)
-    {
-        return NULL;
-    }
+//     new_node = (bin_tree_t *)malloc(sizeof(bin_tree_t));
+//     if (!new_node)
+//     {
+//         return NULL;
+//     }
     
-    printf("Input the data to be stored");
-    scanf("%d", &num);
-    if (num == -1)
-    {
-        return (0);
-    }
+//     printf("Input the data to be stored");
+//     scanf("%d", &num);
+//     if (num == -1)
+//     {
+//         return (0);
+//     }
     
-    new_node->data = num;
-    printf("Input the left node data");
-    new_node->left = create_bin_tree();
-    printf("Input the right node data");
-    new_node->right = create_bin_tree();
+//     new_node->data = num;
+//     printf("Input the left node data");
+//     new_node->left = create_bin_tree();
+//     printf("Input the right node data");
+//     new_node->right = create_bin_tree();
 
-    return (new_node);
-}
+//     return (new_node);
+// }
