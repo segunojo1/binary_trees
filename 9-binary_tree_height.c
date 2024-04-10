a17f33501f48ef00ec356368665d019d34acbf7e
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 23:24:28 +0100

    find height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - return height of the tree
+ * @tree: the pointer to the root
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
+
+    return (left_height > right_height ? left_height + 1 : right_height + 1);
+}
