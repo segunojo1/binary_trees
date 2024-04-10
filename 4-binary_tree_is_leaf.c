da6394369d8463fb93ba8984924a8072eb45e6be
segunojo1 <olusegunkd2017@gmail.com>
2024-04-09 21:04:44 -0700

    betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,16 +7,16 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-    {
-        return (0);
-    }
+	if (!node)
+	{
+		return (0);
+	}
 
-    if (!node->left && !node->right )
-    {
-        return (1);
-    }
-    else{
-        return (0);
-    }
+	if (!node->left && !node->right )
+	{
+		return (1);
+	}
+	else{
+		return (0);
+	}
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,17 +2,17 @@
 /**
  * binary_tree_is_root - checks if a node is the root
  * @node: the node to check
-*/
+ */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-    {
-        return (0);
-    }
-    if (node->parent)
-    {
-        return (0);
-    }
-    return (1);
+	if (!node)
+	{
+		return (0);
+	}
+	if (node->parent)
+	{
+		return (0);
+	}
+	return (1);
 }
