a6e234609cb87b49e3fdf40d2687b0e11fc25cb9
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 22:30:15 +0100

    post order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_inorder - traverses a binary tree in post order
+ * @tree: the pointer to the tree to traverse
+ * @func: func to traverse
+*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (!tree || !func)
---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverses a binary tree in post order
+ * @tree: the pointre to the tree to traverse
+ * @func: func to traverse
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func)
+    {
+        return;
+    };
+    binary_tree_postorder(tree->left, func);
+    binary_tree_postorder(tree->right, func);
+    func(tree->n)
+}
