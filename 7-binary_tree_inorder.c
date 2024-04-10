0dd51eac8908fecb6afe4858d6b3cbe52a3628fa
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 15:01:35 +0100

    inorder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func)
+    {
+        return;
+    }
+
+    binary_tree_inorder(tree->left, func);
+
+    binary_tree_inorder(tree->right, func);
+}
