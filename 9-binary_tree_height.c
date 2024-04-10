1b828c551970d00b071b1b2f43bca1174f6e5559
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 23:47:32 +0100

    edit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,4 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
         size_t right_height = binary_tree_height(tree->right);
         return (left_height > right_height ? left_height + 1 : right_height + 1);
     }
+    return (0);
 }
