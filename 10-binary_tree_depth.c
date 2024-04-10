3c07447c720823cb5b4c18e97344b466c1085765
segunojo1 <olusegunkd2017@gmail.com>
2024-04-10 16:26:53 -0700

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - find sthe depth of a tree
+ * @tree: the pointer to the tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t binary_tree_depth(const binary_tree_t *tree)
+	{
+		binary_tree_t *current;
+		size_t height = 0;
+
+		if (tree == NULL)
+		{
+			return (0);
+		}
+		else
+		{
+			current = tree->parent;
+			while (current != NULL)
+			{
+				height += 1;
+				current = current->parent;
+			}
+		}
+		return (height);
+	}
