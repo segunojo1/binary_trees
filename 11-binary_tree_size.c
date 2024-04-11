fb8cfb28dd39b8a7aad91ef3acfd5833adcc3f54
segunojo1 <olusegunkd2017@gmail.com>
2024-04-11 06:29:09 +0100

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - returns the size of the tree
+ * @tree: the pointer to the tree
+ * 
+ * Return: retur the size
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    size_t height;
+    if (!tree)
+    {
+        return (0);
+    }
+    height += binary_tree_size(tree->left);
+    height += binary_tree_size(tree->right);
+
+    return (height + 1);
+    
+}
