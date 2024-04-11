8076733470980a86bb5623683ec4d9b116e18d70
segunojo1 <olusegunkd2017@gmail.com>
2024-04-11 06:51:01 +0100

    num of nodes

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,7 +7,7 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t height;
+    size_t height = 0;
     if (!tree)
     {
         return (0);
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t nums = 0;
+
+    if (!tree)
+    {
+        return (0);
+    }
+    if (tree->left == NULL && tree->right == NULL)
+    {
+        nums++;
+    }
+    
+    binary_tree_leaves(tree->left);
+    binary_tree_leaves(tree->right);
+
+    return (nums);
+}
